d0569b91a5465812837d7a52a70597aa2db54f9c
tsyn4836 <tsyn4836@gmail.com>
2020-03-07 21:22:28 +0800

    3-1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    app
+  <div id="app" class="g-container">
+    <div class="g-view-container">
+      <router-view></router-view>
+    </div>
+    <div class="g-footer-container">
+      <!-- <c-tabbar/> -->
+    </div>
   </div>
 </template>
 
