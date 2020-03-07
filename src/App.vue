e6856db7065156e9a98c3a65e69aaaa8940acc0e
tsyn4836 <tsyn4836@gmail.com>
2020-03-07 21:59:51 +0800

    项目准备与骨架搭建

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,17 @@
       <router-view></router-view>
     </div>
     <div class="g-footer-container">
-      <!-- <c-tabbar/> -->
+      <c-tabbar/>
     </div>
   </div>
 </template>
 
 <script>
+import CTabbar from 'components/tabbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    CTabbar
+  }
 }
 </script>
